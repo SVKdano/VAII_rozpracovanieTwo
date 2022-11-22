7664516d90ef4ecf78256d6220107a2144252b8f
Daniel Lieskovsky <danol147@gmail.com>
2022-11-22 12:26:08 +0100

    Changes in view

--- php_mysql/www/App/Views/Teams/index.view.php
+++ php_mysql/www/App/Views/Teams/index.view.php
@@ -5,7 +5,9 @@
     <table class="table">
     <thead>
         <tr>
-            <td colspan="4" class="centered table-danger">5.liga</td>
+            <td class="centered table-danger">Nazov timu</td>
+            <td class="centered table-danger">Liga</td>
+            <td class="centered table-danger"></td>
         </tr>
         </thead>
         <tbody>
@@ -18,8 +20,8 @@ foreach ($data as $team) { ?>
             <tr>
                 <td> <?php echo $team->getTeamName(); ?> </td>
                 <td> <?php echo $team->getLeague(); ?> </td>
-                <td> <a href="?c=teams&a=delete&id=<?php echo $team->getId()?>" class="btn btn-danger">Delete</a> </td>
-                <td> <a href="?c=teams&a=edit&id=<?php echo $team->getId()?>" class="btn btn-light">Uprav team</a> </td>
+                <td style="text-align: right"> <a href="?c=teams&a=delete&id=<?php echo $team->getId()?>" class="btn btn-danger">Delete</a>
+                 <a href="?c=teams&a=edit&id=<?php echo $team->getId()?>" class="btn btn-light">Uprav team</a> </td>
             </tr>
 
 <?php
